13db9990826b570563d3a65c832c128fb5470a11
Simon Kellner <the_kellner@web.de>
2020-06-18 12:48:44 +0200

    Fix error from initial files

--- src/main/java/hftstuttgart/Calculator.java
+++ src/main/java/hftstuttgart/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
     }    
 
     public int prod(int op1, int op2) {
-        int sum = 0;
+        int sum = 1;
         sum *= op1;
         sum *= op2;
         return sum;
